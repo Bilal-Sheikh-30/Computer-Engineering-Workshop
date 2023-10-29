411149c6020f194c0f700763e32fc77724794f4b
Bilal-Sheikh-30 <bilalsheikh3011@gmail.com>
2023-10-29 22:17:34 +0500

    added Q#1&4 of lab#2

---
+++ lab#2/q#4.c
@@ -0,0 +1,18 @@
+# include <stdio.h>
+# include <ctype.h>
+
+int main (){
+  char name[] = "ned UNIVERSITYit";
+  for (int i = 0; i < strlen(name); i++){
+    if (islower ( name[i])){
+      name[i] = toupper(name[i]);
+    }else{
+      name[i] = tolower(name[i]);
+      
+    }
+  }
+  printf("edited: %s",name);
+  
+  
+  return 0;
+}
